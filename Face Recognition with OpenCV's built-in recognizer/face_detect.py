c50ed67cb241a12f8f6eafdc9c19f7d79429b3f2
Nwani Ugonna Stanley <64145417+Stan-Leigh@users.noreply.github.com>
2023-04-14 13:31:19 +0200

    Face Recognition with OpenCV's built-in recognizer

---
+++ Face Recognition with OpenCV's built-in recognizer/face_detect.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+img = cv.imread('Photos/group 1.jpg')
+cv.imshow('Group of people', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray People', gray)
+
+# Haar cascades are really sensitive to noise in an image
+# Anything that looks like a face, haar cascade will detect it even if it isn't a face.
+# You can fine tune haar cascade to detect faces better by changing the minNeighbors and scaleFactor parameters
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Faces', img)
+
+
+cv.waitKey(0)
